e685fde6acf4eb9b5eef10a3436189e52e9dd97b
PythonWorld-123/git config --global user.name PythonWorld-123 <anilkhajekar1920@gmail.com>
2020-06-28 23:02:39 +0200

    added driver

--- Sekenium_test.py
+++ Sekenium_test.py
@@ -6,7 +6,7 @@ class Test:
 
    def test_one(self):
      global driver
-     driver=webdriver.Firefox(executable_path='C:\\Users\\Anil\\Desktop\\Firefox_Geco_Driver\\geckodriver.exe')
+     driver=webdriver.Firefox(executable_path='C:\\Users\\Anil\\Desktop\\HQ\\Firefox_Geco_Driver\\geckodriver.exe')
      driver.get("https://www.google.com/")
      driver.maximize_window()
      driver.find_element_by_name("q").send_keys("Hello")
