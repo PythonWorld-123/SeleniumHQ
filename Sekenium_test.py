972880cba032b83c0090bf084d743a559eead0ba
PythonWorld-123/git config --global user.name PythonWorld-123 <anilkhajekar1920@gmail.com>
2020-06-28 21:47:43 +0200

    Commited

---
+++ Sekenium_test.py
@@ -0,0 +1,16 @@
+import pytest
+from selenium import webdriver
+import time
+
+class Test:
+
+   def test_one(self):
+     global driver
+     driver=webdriver.Firefox(executable_path='C:\\Users\\Anil\\Desktop\\Firefox_Geco_Driver\\geckodriver.exe')
+     driver.get("https://www.google.com/")
+     driver.maximize_window()
+     driver.find_element_by_name("q").send_keys("Hello")
+     time.sleep(10)
+     driver.close()
+     
+     
